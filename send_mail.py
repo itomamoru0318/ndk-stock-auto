f64d4425e7a5380789a90106173ac7d346cd779c
itomamoru0318 <ito.mamoru@ndk.com>
2026-04-07 12:51:12 +0900

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -8,10 +8,10 @@ from email.utils import formatdate
 # ===== 設定 =====
 CSV_FILE = "ndk_stock.csv"
 
-# GitHub Secrets から取得
-MAIL_FROM = os.environ["ndk20240318@gmail.com"]
-MAIL_PASSWORD = os.environ["zixq ygbz blaa qewx"]
-MAIL_TO = os.environ["ito.mamoru@ndk.com"]
+# GitHub Secrets から環境変数として取得
+MAIL_FROM = os.environ["MAIL_FROM"]
+MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
+MAIL_TO = os.environ["MAIL_TO"]
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
@@ -25,7 +25,7 @@ with open(CSV_FILE, newline="", encoding="utf-8") as f:
 
 # ===== メール本文作成 =====
 body = f"""
-{latest['Date']} 時点のNDK株価情報をお送りします。
+{latest['Date']} 時点のNDK株価情報です。
 
 【株価】
 Open  : {latest['Open']}
@@ -39,13 +39,13 @@ Close : {latest['Close']}
 
 msg = MIMEText(body, _charset="utf-8")
 msg["Subject"] = "【自動配信】NDK 株価情報"
-msg["From"] = ndk20240318@gmail.com
-msg["To"] = ito.mamoru@ndk.com
+msg["From"] = MAIL_FROM
+msg["To"] = MAIL_TO
 msg["Date"] = formatdate(localtime=True)
 
 # ===== メール送信 =====
 with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:
-    smtp.login(ndk20240318@gmail.com, zixq ygbz blaa qewx)
+    smtp.login(MAIL_FROM, MAIL_PASSWORD)
     smtp.send_message(msg)
 
 print("Mail sent successfully")
